6b60cc887a7b3ff186961cc9cdce96d51c314432
Andreas Mayer <andreas.mayer@inn2.de>
2024-08-21 07:52:32 +0000

    Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("hap.zvaadfvk00005.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
