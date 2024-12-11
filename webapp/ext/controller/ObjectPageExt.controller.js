4a9cbdbbf395eb18437a18863cc94912686d191d
Andreas Mayer <andreas.mayer@inn2.de>
2024-12-11 11:21:26 +0000

    11.12.24_updateDataAfterActions

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -6,6 +6,7 @@ sap.ui.define([], function () {
 		
 		onAfterRendering: function () {
 			this.setButtonActionType();
+			this.updateDataAfterActions();
 		},
 
 		setButtonActionType() {
@@ -13,6 +14,38 @@ sap.ui.define([], function () {
             var moveToTargetBtn = this.getView().byId("CustomersTourSource::action::cds_zsd_vaa_dfvk_ui_fv03.cds_zsd_vaa_dfvk_ui_fv03_Entities::MoveToTarget");
             moveToSourceBtn.setType("Emphasized");
             moveToTargetBtn.setType("Emphasized");
+        },
+
+		updateDataAfterActions() {
+			var oModel = this.getView().getModel();
+			oModel.attachRequestCompleted(function (evn) {
+				var tourSourceTable = this.getView().byId('CustomersTourSource::Table');
+				var tourTargetTable = this.getView().byId('CustomerTourTarget::Table');
+
+				if (evn.getParameter("url").startsWith("MoveToTarget") && evn.getParameter("success")) {
+					tourTargetTable.rebindTable();
+				} else if (evn.getParameter("url").startsWith("MoveToSource") && evn.getParameter("success")) {
+					tourSourceTable.rebindTable();
+				} 
+			}.bind(this));
+
+			/* 
+			var moveToSourceBtn = this.getView().byId("CustomerTourTarget::action::cds_zsd_vaa_dfvk_ui_fv03.cds_zsd_vaa_dfvk_ui_fv03_Entities::MoveToSource");
+            var moveToTargetBtn = this.getView().byId("CustomersTourSource::action::cds_zsd_vaa_dfvk_ui_fv03.cds_zsd_vaa_dfvk_ui_fv03_Entities::MoveToTarget");
+
+			moveToSourceBtn.attachValidationSuccess((evn)=>{
+				debugger
+			});
+			moveToSourceBtn.attachValidationError((evn)=>{
+				debugger
+			});
+			moveToTargetBtn.attachValidationSuccess((evn)=>{
+				debugger
+			});
+			moveToTargetBtn.attachValidationError((evn)=>{
+				debugger
+			}); 
+			*/
         }
 		
 	};
